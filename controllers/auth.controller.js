f4167e9b607fa202ccd004e813becbea05fdfbe5
Monica <monica.parrillo@gmail.com>
2020-12-20 17:38:28 -0600

    format the code

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -11,16 +11,18 @@ const bcrypt = require('bcryptjs')
 
 
 //this will handle sign up
-exports.signup = (req,res) => {
+exports.signup = (req, res) => {
   // we are going to make our user object using paramms from req
   const user = new User({
     username: req.body.username,
     email: req.body.email,
     password: bcrypt.hashSync(req.body.password, 8)
   })
-  user.save((err,user)=>{
-    if(err){
-      return res.status(500).send({message: 'Error signing up user'+err})
+  user.save((err, user) => {
+    if (err) {
+      return res.status(500).send({
+        message: 'Error signing up user' + err
+      })
     }
     //this will be sent with the response
     const userData = {
@@ -32,36 +34,48 @@ exports.signup = (req,res) => {
     }
 
     // we check if roles were passed on req.body
-    if(req.body.roles){
+    if (req.body.roles) {
       Role.find({
-        name: {$in: req.body.roles}
-      },(err,roles) => {
-        if(err){
-          return res.status(500).send({message: err})
+        name: {
+          $in: req.body.roles
+        }
+      }, (err, roles) => {
+        if (err) {
+          return res.status(500).send({
+            message: err
+          })
         }
         // pass roles from the request
-        user.roles = roles.map(role=>role._id)
-        roles.forEach(role=>{
+        user.roles = roles.map(role => role._id)
+        roles.forEach(role => {
           userData.roles.push(`ROLE_${role.name.toUpperCase()}`)
         })
-        user.save( async (err,savedUser)=>{
-          if(err){
-            res.status(500).send({message:err})
+        user.save(async (err, savedUser) => {
+          if (err) {
+            res.status(500).send({
+              message: err
+            })
           }
-            res.status(200).send(userData)
+          res.status(200).send(userData)
         })
       })
     } else {
       // default: user that doesn't pass roles gets a user role
-      Role.findOne({name:"user"},(err,role)=>{
-        if(err){
-          res.status(500).send({message:err})
+      Role.findOne({
+        name: "user"
+      }, (err, role) => {
+        if (err) {
+          res.status(500).send({
+            message: err
+          })
           return
         }
         user.roles = [role._id]
-        user.save(async err=>{
-          if(err){
-            return res.status(500).send({message:err})
+        user.save(async err => {
+          if (err) {
+            return res.status(500).send({
+              message: err
+            })
           }
           userData.roles.push("ROLE_USER")
           res.status(200).send(userData)
@@ -74,50 +88,59 @@ exports.signup = (req,res) => {
 
 
 const getJWT = (user) => {
-  return jwt.sign({id:user._id},config.secret,{
+  return jwt.sign({
+    id: user._id
+  }, config.secret, {
     expiresIn: 86400 // expires today in 24 hours (in seconds)
   })
 }
 
 
-exports.signin = (req,res) => {
+exports.signin = (req, res) => {
   User.findOne({
-    username: req.body.username
-  })
-  // populates values from the roles id we stored in the document
-  .populate('roles',"-__v")
-  // exec returning our user
-  .exec((err, user) => {
-    if(err){
-      res.status(500).send({message:err})
-      return
-    }
-    //user did not exist
-    if(!user){
-      res.status(404).send({message: "User not found"})
-      return
-    }
-    // validate the password via bcrypt
-    const passwordIsValid = bcrypt.compareSync(req.body.password, user.password)
-    // if password not valid, let the user know
-    if(!passwordIsValid){
-      return res.status(401).send({accessToken: null, message: "invalid password"})
-    }
-    // if password is valid, generate a token
-    const token = getJWT(user)
-    // setting roles to pass back in our response
-    let authorities = [];
-    for (let i =0;i<user.roles.length;i++){
-      authorities.push("ROLE_" + user.roles[i].name.toUpperCase())
-    }
-
-
-    res.status(200).send({
-      id: user._id,
-      username: user.username,
-      email: user.email,
-      roles: authorities,
-      accessToken: token
+      username: req.body.username
     })
-  })
+    // populates values from the roles id we stored in the document
+    .populate('roles', "-__v")
+    // exec returning our user
+    .exec((err, user) => {
+      if (err) {
+        res.status(500).send({
+          message: err
+        })
+        return
+      }
+      //user did not exist
+      if (!user) {
+        res.status(404).send({
+          message: "User not found"
+        })
+        return
+      }
+      // validate the password via bcrypt
+      const passwordIsValid = bcrypt.compareSync(req.body.password, user.password)
+      // if password not valid, let the user know
+      if (!passwordIsValid) {
+        return res.status(401).send({
+          accessToken: null,
+          message: "invalid password"
+        })
+      }
+      // if password is valid, generate a token
+      const token = getJWT(user)
+      // setting roles to pass back in our response
+      let authorities = [];
+      for (let i = 0; i < user.roles.length; i++) {
+        authorities.push("ROLE_" + user.roles[i].name.toUpperCase())
+      }
+
+
+      res.status(200).send({
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        roles: authorities,
+        accessToken: token
+      })
+    })
 }
